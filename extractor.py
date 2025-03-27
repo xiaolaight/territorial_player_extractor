0a928d8d38c4bceeb4d0925adc6c4781bdbda9b4
xiaolaight <la1ght.du@gmail.com>
2025-03-27 14:28:46 -0400

    Update extractor.py
    function updated, loop assigned, fix needed: parse the 1v1 strings to remove the floats within the brackets

--- extractor.py
+++ extractor.py
@@ -4,6 +4,27 @@ from selenium.webdriver.support import expected_conditions as EC
 
 url = "https://territorial.io"
 
+style_args = {
+
+    "color": "rgb(170, 170, 170)",
+    "cursor": "pointer",
+    "margin-top": "0.2em",
+    "margin-right": "0.2em",
+    "margin-bottom": "0.2em",
+    "max-width": "9em",
+    "width": "9em",
+    "max-height": "1.4em",
+    "height": "1.4em",
+    "white-space-collapse": "collapse",
+    "text-wrap-mode": "nowrap",
+    "overflow-x": "hidden",
+    "overflow-y": "hidden",
+    "text-overflow": "ellipsis",
+    "font": "inherit",
+    "display": "inline-block"
+
+}
+
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver=webdriver.Chrome(options=options)
@@ -11,23 +32,36 @@ driver.get(url)
 multiplayer_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable(("xpath", "//html/body/button[1]")))
 multiplayer_button.click()
 
-driver.implicitly_wait(20)
-spans = driver.find_elements("xpath", "//html/body/div[1]/div[5]/div/div")
-for elem in spans:
-    s = elem.text
+teamPath = "//html/body/div[1]/div[2]/div/button[1]"
+battlePath = "//html/body/div[1]/div[2]/div/button[2]"
+vsPath = "//html/body/div[1]/div[2]/div/button[3]"
+zombiePath = "//html/body/div[1]/div[2]/div/button[4]"
 
-res = [ ]
-cur = [ ]
+allPath = [teamPath, battlePath, vsPath, zombiePath]
 
-for i in range(len(s)):
-    if ((s[i] == '🟢' or s[i] == '⚪') and len(cur) > 0):
-        cs = ''.join(cur)
-        res.append(cs)
-        cur = []
-    elif (s[i] == '🟢' or s[i] == '⚪'):
-        continue
-    else:
-        cur.append(s[i])
+def pageRead(desiredPath):
+    multiplayer_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable(("xpath", desiredPath)))
+    multiplayer_button.click()
+    driver.implicitly_wait(20)
+    spans = driver.find_elements("xpath", "//html/body/div[1]/div[5]/div/div")
+    for elem in spans:
+        allPlayers = elem.text
 
-for elem in res:
-    print(elem)
+    result = [ ]
+    cur = [ ]
+
+    for i in range(len(allPlayers)):
+        if ((allPlayers[i] == '🟢' or allPlayers[i] == '⚪') and len(cur) > 0):
+            cs = ''.join(cur)
+            result.append(cs)
+            cur = []
+        elif (allPlayers[i] == '🟢' or allPlayers[i] == '⚪'):
+            continue
+        else:
+            cur.append(allPlayers[i])
+
+    for elem in result:
+        print(elem)
+
+for i in range(4):
+    pageRead(allPath[i])
