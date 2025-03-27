18a1e012184cce22dc5fa3e4e989e5f1e6988fa2
xiaolaight <la1ght.du@gmail.com>
2025-03-27 14:19:48 -0400

    Update extractor.py
    fixed issues, core of the function is present, final version updated next

--- extractor.py
+++ extractor.py
@@ -1,33 +1,36 @@
 from bs4 import BeautifulSoup
-from requests_html import HTMLSession
+import requests
 from selenium import webdriver
-import pandas as pd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 url = "https://territorial.io"
 
-style_args = {
-
-    "color" : "rgb(170, 170, 170)",
-    "cursor" : "pointer",
-    margin - top: 0.2em
-    margin - right: 0.2em
-    margin - bottom: 0.2em
-    max - width: 9em
-    width: 9em
-    max - height: 1.4em
-    height: 1.4em
-    white-space-collapse: collapse
-    text-wrap-mode: nowrap
-    overflow-x: hidden
-    overflow-y: hidden
-    text-overflow: ellipsis
-    font: inherit;
-    display: inline - block
-
-}
-
-driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver=webdriver.Chrome(options=options)
 driver.get(url)
-driver.find_elements_by_xpath("//*[contains(text(), 'Multiplayer')]").click()
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-players = soup.find_all(attrs={})
+multiplayer_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable(("xpath", "//html/body/button[1]")))
+multiplayer_button.click()
+
+driver.implicitly_wait(20)
+spans = driver.find_elements("xpath", "//html/body/div[1]/div[5]/div/div")
+for elem in spans:
+    s = elem.text
+
+res = [ ]
+cur = [ ]
+
+for i in range(len(s)):
+    if ((s[i] == '🟢' or s[i] == '⚪') and len(cur) > 0):
+        cs = ''.join(cur)
+        res.append(cs)
+        cur = []
+    elif (s[i] == '🟢' or s[i] == '⚪'):
+        continue
+    else:
+        cur.append(s[i])
+
+for elem in res:
+    print(elem)
