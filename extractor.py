aafe3220f831281acde880d4abedc0fb616eb3aa
jfiwe <65134288+jfiwae@users.noreply.github.com>
2025-03-27 21:27:40 -0400

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from github import Github
+from github import Auth
+import time
 
 url = "https://territorial.io"
 
@@ -18,6 +21,21 @@ zombiePath = "//html/body/div[1]/div[2]/div/button[4]"
 
 allPath = [teamPath, battlePath, vsPath, zombiePath]
 
+auth = Auth.Token("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")
+g = Github(auth=auth)
+repo = g.get_repo("xiaolaight/territorial_player_extractor")
+userList = repo.get_contents("names.txt")
+names = str(userList.decoded_content)
+targetUsers = [ ]
+c = ""
+for i in range(len(names)):
+    if (names[i] == "`"):
+        targetUsers.append(c)
+        c = ""
+    else:
+        c+=names[i]
+del targetUsers[0]
+
 def pageRead(desiredPath):
     multiplayer_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable(("xpath", desiredPath)))
     multiplayer_button.click()
@@ -29,18 +47,20 @@ def pageRead(desiredPath):
     result = [ ]
     cur = [ ]
 
+
     for i in range(len(allPlayers)):
         if ((allPlayers[i] == '🟢' or allPlayers[i] == '⚪') and len(cur) > 0):
             cs = ''.join(cur)
-            result.append(cs)
+            for j in targetUsers:
+                if j in cs:
+                    outputFile = repo.get_contents("detect.txt")
+                    originalText = str(outputFile.decoded_content)
+                    newText = "User " + cs + " detected at " + str(time.ctime()) + " with keyword " + j + '\n'
+                    repo.update_file(outputFile.path, "NEW COMMIT", f"{originalText} {newText}", outputFile.sha)
             cur = []
         elif (allPlayers[i] == '🟢' or allPlayers[i] == '⚪'):
             continue
         else:
             cur.append(allPlayers[i])
 
-    for elem in result:
-        print(elem)
-
-for i in range(4):
-    pageRead(allPath[i])
+pageRead(battlePath)
