11ad6d0f949d036d4d562dc0bc6b44519c98a227
xiaolaight <la1ght.du@gmail.com>
2025-03-27 22:10:28 -0400

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -28,19 +28,18 @@ userList = repo.get_contents("names.txt")
 names = str(userList.decoded_content)
 targetUsers = names.split('`')
 del targetUsers[0]
-print(targetUsers)
+targetUsers.pop()
 
 def pageRead(desiredPath):
     multiplayer_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable(("xpath", desiredPath)))
     multiplayer_button.click()
     driver.implicitly_wait(20)
+
     spans = driver.find_elements("xpath", "//html/body/div[1]/div[5]/div/div")
     for elem in spans:
         allPlayers = elem.text
 
-    result = [ ]
     cur = [ ]
-
 
     for i in range(len(allPlayers)):
         if ((allPlayers[i] == '🟢' or allPlayers[i] == '⚪') and len(cur) > 0):
@@ -49,7 +48,6 @@ def pageRead(desiredPath):
                 if j in cs:
                     outputFile = repo.get_contents("detect.txt")
                     originalText = str(outputFile.decoded_content.decode("utf-8"))
-                    print(originalText)
                     newText = "User " + cs + " detected at " + str(time.ctime()) + " with keyword " + j + "  \n"
                     repo.update_file(outputFile.path, "NEW COMMIT", f"{originalText} {newText}", outputFile.sha)
             cur = []
@@ -58,4 +56,10 @@ def pageRead(desiredPath):
         else:
             cur.append(allPlayers[i])
 
-pageRead(battlePath)
+def runAll():
+    while True:
+        for i in range(0, 4):
+            pageRead(allPath[i])
+        time.sleep(10)
+
+runAll()
